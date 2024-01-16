402f4ff4bdb872a51b0ae4d56860cd84f1f087fb
ysmoser <90534144+ysmoser@users.noreply.github.com>
2024-01-16 16:17:25 -0700

    Fixed Javascript for Desgin Principles Document

--- lesson02/scripts/lastmodified.js
+++ lesson02/scripts/lastmodified.js
@@ -1,4 +1,4 @@
 let currentYear = new Date().getFullYear();
 document.getElementById("year").innerHTML = currentYear;
 let modifiedDate = document.lastModified;
-document.getElementById('lastModified').innerHTML = `Last Modification: ${modifiedDate}`;
+document.getElementById('lastModified').innerHTML = `Last Updated: ${modifiedDate}`;
