ae3e33188673b8f8a2c56f0a4f3a7492b65d798d
ysmoser <90534144+ysmoser@users.noreply.github.com>
2024-01-23 12:15:40 -0700

    added a hamburger icon on the main page on mobile. working on the google map.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,3 +2,23 @@ let currentYear = new Date().getFullYear();
 document.getElementById("year").innerHTML = currentYear;
 let modifiedDate = document.lastModified;
 document.getElementById('lastModified').innerHTML = `Last Modification: ${modifiedDate}`;
+
+// Add a hamburger menu icon 
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+// Remove the "open" class when the screen is bigger than 600px
+function closeMenuOnResize() {
+    if (window.innerWidth > 600) {
+        navigation.classList.remove('open');
+        hamButton.classList.remove('open');
+    }
+}
+
+window.addEventListener('resize', closeMenuOnResize);
