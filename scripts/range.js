7447cf5ee5cec7fff9694551e23141e8f8959d93
ysmoser <90534144+ysmoser@users.noreply.github.com>
2024-02-29 11:48:21 -0700

    For week 08 assignment, I added range.js and connected to form.html.

---
+++ scripts/range.js
@@ -0,0 +1,15 @@
+// Week 08 Page Feedback Form
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+
+
