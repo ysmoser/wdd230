704b0b345cf042533da7a8d9129e414764cad853
ysmoser <90534144+ysmoser@users.noreply.github.com>
2024-02-29 10:45:22 -0700

    Fixed week 6 chamber assignment on windchill.

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,11 +1,20 @@
+
 function calculateWindChill() {
-    const temperature = parseFloat(document.getElementById("temperatureInput").value);
-    const windSpeed = parseFloat(document.getElementById("windSpeedInput").value);
+
+    //const temperature = parseFloat(document.getElementById("temperatureInput").value);
+    //const windSpeed = parseFloat(document.getElementById("windSpeedInput").value);
+    const temperature = 40;
+    const windSpeed = 5;
+
     const windChillElement = document.getElementById("windChillValue");
     if (temperature <= 50 && windSpeed > 3.0) {
         const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
-        windChillElement.textContent = "Wind Chill: " + windChill.toFixed(2);
+        windChillElement.textContent = "Wind Chill: " + windChill.toFixed(2) + " F";
     } else {
         windChillElement.textContent = "N/A";
     }
 }
+
+window.onload = function () {
+    calculateWindChill();
+};
