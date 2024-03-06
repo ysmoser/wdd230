dfa16e47b79f8a244bcb705d85661a7a3a254fa6
ysmoser <90534144+ysmoser@users.noreply.github.com>
2024-03-05 17:19:28 -0700

    Added more function to week 10 learning activities.

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,8 +1,8 @@
 // select HTML elements in the document
 
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+//const weatherIcon = document.querySelector('#weather-icon');
+//const captionDesc = document.querySelector('figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=8af9626bf1661ea0eedbcd82ab4f1be4';
 async function apiFetch() {
@@ -10,7 +10,8 @@ async function apiFetch() {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            //console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -20,4 +21,51 @@ async function apiFetch() {
     }
 }
 
+// Add catpitalization to the first letter of each word
+function capEachWord(description) {
+    return description
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ');
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
+
+
+    let weatherCond = document.createElement('div');
+
+    data.weather.forEach((event) => {
+        const iconsrc = `https://openweathermap.org/img/w/${event.icon}.png`;
+        let desc = capEachWord(event.description);
+
+
+        let weatherEl = document.createElement('figure');
+        let iconEl = document.createElement('img');
+        let captionEl = document.createElement('figcaption');
+
+        iconEl.setAttribute('src', iconsrc);
+        iconEl.setAttribute('alt', desc);
+        captionEl.textContent = desc;
+
+        weatherEl.appendChild(iconEl);
+        weatherEl.appendChild(captionEl);
+
+        weatherCond.appendChild(weatherEl);
+    });
+
+    currentTemp.parentElement.appendChild(weatherCond);
+}
+
 apiFetch();
+
+/*
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+  } 
+*/
